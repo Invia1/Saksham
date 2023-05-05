11f71574b0d7020241d0a41d3a30691015c69d65
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-05 14:56:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static double salary;
+        static string name = "Saksham";
+        static void Main(string[] args)
+        {
+            Program.salary = 10000;
+            Console.WriteLine(Program.name +" "+ "Salary is " + Program.salary);
+        }
+    }
+}
