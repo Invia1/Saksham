01cd03b98b4ef5b4b28edb056452f39eca4263f5
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-05 15:03:47 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3_Palindrome_String_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string");
+            String name = Console.ReadLine();
+            String reverse = String.Empty;
+            for (int i = name.Length - 1 ; i >= 0; i--)
+            {
+                reverse = reverse + name[i];
+            } 
+            if (reverse == name)
+            {
+                Console.WriteLine(name + " " + "is palindrome");
+            }
+            else
+            {
+                Console.WriteLine(name + " " + "is not palindrome");
+            }
+        }
+    }
+}
