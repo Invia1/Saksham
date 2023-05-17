ddaee1b8fd1b2958bf178a2c02a7729c86e9ba3c
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-17 08:53:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+
+namespace ConsoleApp1
+{
+    class data_entry
+    {
+        ArrayList array = new ArrayList();
+
+        public void add_data()
+        {
+            array.Add("Saksham Choudhary");
+            array.Add("Lokesh Rana");
+            array.Add("Jatin Saini");
+           
+            array.Insert(0, "Raman"); 
+
+
+        }
+        public void remove_data()
+        {
+           
+            Console.WriteLine("The updated array list:");
+            array.Remove("Lokesh Rana");
+            array.RemoveAt(2);
+
+
+        }
+        public void print()
+        {
+            foreach (var temp in array)
+            {
+                Console.WriteLine(temp);
+            }
+        }
+
+       
+        public static void Main()
+        {
+            var obj = new data_entry();
+            obj.add_data();
+            obj.print();
+
+            obj.remove_data();
+            obj.print();
+
+
+            Console.ReadLine();
+        }
+    }
+}
